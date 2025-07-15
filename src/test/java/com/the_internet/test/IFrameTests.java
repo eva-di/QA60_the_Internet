2389db8e8a3defd76ea1782d2eb05733601925c2
Eva Dieterle <eva.dieterle.esl@gmail.com>
2025-07-15 09:02:48 +0200

    Homework IFRAMES

---
+++ src/test/java/com/the_internet/test/IFrameTests.java
@@ -0,0 +1,20 @@
+package com.the_internet.test;
+
+import com.the_Internet.pages.HomePage;
+import com.the_Internet.pages.frames.IFramesPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class IFrameTests extends TestBase {
+
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver).selectFrames();
+        new IFramesPage(driver).selectIFrames();
+    }
+
+    @Test
+    public void iFramesTests() {
+        new IFramesPage(driver).closeNotification().verifyContentByText("Your content goes here.");
+    }
+}
---
+++ src/test/java/com/the_internet/test/NestedFramesTests.java
@@ -0,0 +1,21 @@
+package com.the_internet.test;
+
+import com.the_Internet.pages.HomePage;
+import com.the_Internet.pages.frames.NestedFramesPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class NestedFramesTests extends TestBase {
+
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver).selectFrames();
+        new NestedFramesPage(driver).selectNestedFrames();
+    }
+
+    @Test
+    public void nestedFramesTest() {
+        new NestedFramesPage(driver).returnListOfFrames();
+        new NestedFramesPage(driver).verifyFrameContent("BOTTOM");
+    }
+}
